a2a796f40cf9bed5737c45001f1b802a9a0cce57
ola wozniak <olawozniak551@gmail.com>
2022-05-19 13:25:37 +0200

    task 18.3

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -3,22 +3,33 @@ import {Grid,Row,Col} from 'react-flexbox-grid';
 import OrderOption from '../OrderOption/OrderOption';
 import OrderSummary from '../OrderSummary/OrderSummary';
 import pricing from '../../../data/pricing.json';
+import PropTypes from 'prop-types';
 
 
-
-const OrderForm = ({cost,id}) =>(
+const OrderForm = ({tripCost, options, setOrderOption}) =>{
+    return (
 <Grid>
   <Row>
-  {pricing.map(options =>(
-          <Col md={4} key={id}>
-              <OrderOption {...options} /> 
-          </Col>
+  {pricing.map(option =>(
+          <Col md={4} key={option.id}>
+              <OrderOption {...option} currentValue={options[option.id]} setOrderOption=
+              {setOrderOption}/> 
+              </Col>
       ))};
     <Col xs={12}>
-      <OrderSummary tripCost={cost} tripOptions={pricing}/>
+      <OrderSummary cost={tripCost} options={options}/>
     </Col>
   </Row>
 </Grid>
 );
+};
+OrderForm.propTypes = {
+    tripCost: PropTypes.string,
+    options:PropTypes.object,
+    setOrderOption: PropTypes.func,
+
+}
+
+
 
 export default OrderForm;
---
+++ src/components/features/OrderOption/OrderOptionIcons.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import styles from './OrderOption.module.scss';
+import {formatPrice} from '../../../utils/formatPrice';
+import PropTypes from 'prop-types';
+import Icon from'../../common/Icon/Icon';
+const OrderOptionIcons = ({values, required, currentValue, setOptionValue}) => (
+    <div className={styles.component}>
+      {required ? '' : (
+        <div>
+         <Icon name='times-circle'/>
+         <span>none</span>
+        </div>
+      )}
+      {values.map(value => (
+        <div className={currentValue === value.id ? styles.iconActive : styles.icon} 
+        key={value.id}
+        onClick={() => setOptionValue(value.id)}>
+            <Icon name={value.icon}/>
+            {value.name}({formatPrice(value.price)})
+
+        </div>
+      ))}
+    </div>
+  );
+
+
+  OrderOptionIcons.propTypes = {
+    values: PropTypes.array,
+    required: PropTypes.bool,
+    currentValue: PropTypes.string,
+    setOptionValue: PropTypes.func,
+  };
+
+export default OrderOptionIcons;
