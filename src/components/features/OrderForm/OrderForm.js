9d9b5cc98be4634212d425403639a7db885cb8be
ola wozniak <olawozniak551@gmail.com>
2022-05-19 12:32:17 +0200

    task 18.2

---
+++ src/components/features/OrderForm/OrderForm.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Grid,Row,Col} from 'react-flexbox-grid';
+import OrderOption from '../OrderOption/OrderOption';
+import OrderSummary from '../OrderSummary/OrderSummary';
+import pricing from '../../../data/pricing.json';
+
+
+
+const OrderForm = ({cost,id}) =>(
+<Grid>
+  <Row>
+  {pricing.map(options =>(
+          <Col md={4} key={id}>
+              <OrderOption {...options} /> 
+          </Col>
+      ))};
+    <Col xs={12}>
+      <OrderSummary tripCost={cost} tripOptions={pricing}/>
+    </Col>
+  </Row>
+</Grid>
+);
+
+export default OrderForm;
