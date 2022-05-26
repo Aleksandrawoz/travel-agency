6da59b74a20c6a879ce9a6cdad1bef1d5c456100
ola wozniak <olawozniak551@gmail.com>
2022-05-26 07:41:58 +0200

    task 19.4

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -4,9 +4,51 @@ import OrderOption from '../OrderOption/OrderOption';
 import OrderSummary from '../OrderSummary/OrderSummary';
 import pricing from '../../../data/pricing.json';
 import PropTypes from 'prop-types';
+import Button from '../../common/Button/Button';
+import settings from '../../../data/settings';
+import {formatPrice} from '../../../utils/formatPrice';
+import {calculateTotal} from '../../../utils/calculateTotal';
 
-
-const OrderForm = ({tripCost, options,setOrderOption }) =>{
+const sendOrder = (options, tripCost, tripId, tripName, country) => {
+  const totalCost = formatPrice(calculateTotal(tripCost, options));
+  
+  const payload = {
+    ...options,
+    totalCost,
+    tripId,
+    tripName,
+    country,
+  };
+  
+  const url = settings.db.url + '/' + settings.db.endpoint.orders;
+  
+  const fetchOptions = {
+    cache: 'no-cache',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  };
+  
+  fetch(url, fetchOptions)
+    .then(function(response){
+      return response.json();
+    }).then(function(parsedResponse){
+      console.log('parsedResponse', parsedResponse);
+    });
+};
+const checkData = (options, tripCost, tripId, tripName, country) => {
+  if(options.name && options.contact) {
+    sendOrder(options, tripCost, tripId, tripName, country);
+    window.alert('order confirm');
+  }
+  else {
+    window.alert('order incomplete');
+  }
+};
+  
+const OrderForm = ({tripCost, options,setOrderOption,tripId, tripName, country}) =>{
   return (
     <Grid>
       <Row>
@@ -18,6 +60,7 @@ const OrderForm = ({tripCost, options,setOrderOption }) =>{
         <Col xs={12}>
           <OrderSummary cost={tripCost} options={options}/>
         </Col>
+        <Button onClick={() => checkData(options, tripCost,tripId, tripName, country )}>Order now!</Button>
       </Row>
     </Grid>
   );
@@ -26,6 +69,9 @@ OrderForm.propTypes = {
   tripCost: PropTypes.string,
   options:PropTypes.object,
   setOrderOption:PropTypes.func,
+  tripId: PropTypes.string,
+  tripName: PropTypes.string,
+  country: PropTypes.string,
 
 };
 
